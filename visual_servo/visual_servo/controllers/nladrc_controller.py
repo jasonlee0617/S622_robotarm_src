b68c3d136e19e406ced348f1f30429e8e7caa091
jasonlee0617 <676983492@qq.com>
2026-06-12 23:41:19 +0800

    2026 06.12 23:41

--- visual_servo/visual_servo/controllers/nladrc_controller.py
+++ visual_servo/visual_servo/controllers/nladrc_controller.py
@@ -58,6 +58,9 @@ class NLADRC_1st_Order:
         delta_ctrl: float,
         err_transition: float,
         obs_error_clip: float,
+        obs_transition: float,
+        z2_clip: float,
+        u_fb_clip: float,
         u_rate_max: Optional[float] = None,
         u_ema_alpha: float = 1.0,
         u_clip: Optional[float] = None,
@@ -73,6 +76,9 @@ class NLADRC_1st_Order:
         self.delta_ctrl = float(delta_ctrl)
         self.err_transition = float(err_transition)
         self.obs_error_clip = float(obs_error_clip)
+        self.obs_transition = float(obs_transition)
+        self.z2_clip = float(z2_clip)
+        self.u_fb_clip = float(u_fb_clip)
         self.u_rate_max = None if u_rate_max is None else float(u_rate_max)
         self.u_ema_alpha = float(u_ema_alpha)
         self.u_clip = None if u_clip is None else float(u_clip)
@@ -85,6 +91,12 @@ class NLADRC_1st_Order:
             raise ValueError("err_transition must be > 0")
         if self.obs_error_clip <= 0.0:
             raise ValueError("obs_error_clip must be > 0")
+        if self.obs_transition <= 0.0:
+            raise ValueError("obs_transition must be > 0")
+        if self.z2_clip <= 0.0:
+            raise ValueError("z2_clip must be > 0")
+        if self.u_fb_clip <= 0.0:
+            raise ValueError("u_fb_clip must be > 0")
         if self.u_rate_max is not None and self.u_rate_max <= 0.0:
             raise ValueError("u_rate_max must be > 0 when set")
         if not (0.0 < self.u_ema_alpha <= 1.0):
@@ -104,13 +116,17 @@ class NLADRC_1st_Order:
     def step(self, error: float, u_ff: float = 0.0) -> float:
         e_obs = self.z1 - float(error)
         e_obs = float(np.clip(e_obs, -self.obs_error_clip, self.obs_error_clip))
+        obs_mix = float(np.clip(abs(e_obs) / self.obs_transition, 0.0, 1.0))
         f_obs = fal(e_obs, self.alpha_obs, self.delta_obs)
         f_obs2 = fal(e_obs, self.alpha_obs2, self.delta_obs)
+        obs_term1 = (1.0 - obs_mix) * e_obs + obs_mix * f_obs
+        obs_term2 = (1.0 - obs_mix) * e_obs + obs_mix * f_obs2
 
-        z1_dot = self.z2 - self.b0 * self.u_applied_last - self.beta1 * f_obs
-        z2_dot = -self.beta2 * f_obs2
+        z1_dot = self.z2 - self.b0 * self.u_applied_last - self.beta1 * obs_term1
+        z2_dot = -self.beta2 * obs_term2
         self.z1 += z1_dot * self.dt
         self.z2 += z2_dot * self.dt
+        self.z2 = float(np.clip(self.z2, -self.z2_clip, self.z2_clip))
 
         err_abs = abs(self.z1)
         nonlinear_mix = float(np.clip(err_abs / self.err_transition, 0.0, 1.0))
@@ -119,8 +135,10 @@ class NLADRC_1st_Order:
         u0_linear = self.wc * self.z1
         u0_nonlinear = self.wc * f_ctrl
         u0 = linear_mix * u0_linear + nonlinear_mix * u0_nonlinear
-        u_fb = (u0 + self.z2) / self.b0
-        u_cmd_pre = float(u_fb + float(u_ff))
+        dist_weight = float(max(0.0, 1.0 - abs(e_obs) / self.obs_error_clip))
+        u_fb = (u0 + dist_weight * self.z2) / self.b0
+        u_fb_bounded = float(np.clip(u_fb, -self.u_fb_clip, self.u_fb_clip))
+        u_cmd_pre = float(u_fb_bounded + float(u_ff))
         u = float(u_cmd_pre)
 
         if self.u_rate_max is not None:
@@ -136,13 +154,13 @@ class NLADRC_1st_Order:
             z1=float(self.z1),
             z2=float(self.z2),
             u0=float(u0),
-            u_fb=float(u_fb),
+            u_fb=float(u_fb_bounded),
             u_ff=float(u_ff),
             u_cmd_pre=float(u_cmd_pre),
             u_cmd_shaped=float(u),
             u_applied_last=float(self.u_applied_last),
             u=float(u),
-            fal_obs=float(f_obs),
+            fal_obs=float(obs_term1),
             fal_ctrl=float(f_ctrl),
             linear_mix=float(linear_mix),
             e_obs=float(e_obs),
@@ -180,6 +198,12 @@ class NLADRCController3D:
         err_transition_z=0.004,
         obs_error_clip_xy=0.02,
         obs_error_clip_z=0.01,
+        obs_transition_xy=0.004,
+        obs_transition_z=0.002,
+        z2_clip_xy=0.12,
+        z2_clip_z=0.08,
+        u_fb_clip_xy=0.28,
+        u_fb_clip_z=0.10,
         u_rate_max_xy=0.60,
         u_rate_max_z=0.20,
         u_ema_alpha=0.35,
@@ -197,6 +221,9 @@ class NLADRCController3D:
             delta_ctrl_xy,
             err_transition_xy,
             obs_error_clip_xy,
+            obs_transition_xy,
+            z2_clip_xy,
+            u_fb_clip_xy,
             u_rate_max_xy,
             u_ema_alpha,
             u_clip_xy,
@@ -213,6 +240,9 @@ class NLADRCController3D:
             delta_ctrl_xy,
             err_transition_xy,
             obs_error_clip_xy,
+            obs_transition_xy,
+            z2_clip_xy,
+            u_fb_clip_xy,
             u_rate_max_xy,
             u_ema_alpha,
             u_clip_xy,
@@ -229,6 +259,9 @@ class NLADRCController3D:
             delta_ctrl_xy,
             err_transition_z,
             obs_error_clip_z,
+            obs_transition_z,
+            z2_clip_z,
+            u_fb_clip_z,
             u_rate_max_z,
             u_ema_alpha,
             None,
